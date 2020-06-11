665ea2d7d72efb3ea83b00227fd27cbc22511e25
elias <elias.jm.garcia@gmail.com>
2020-06-11 16:19:11 -0500

    learned about functional components, extracted render logic out of Popular's render into a functional component

--- gh-battle/app/components/Popular.js
+++ gh-battle/app/components/Popular.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+function LangNav({ selected, updateLang}) {
+  const languages = ['all', 'js', 'rb', 'java', 'css', 'py', 'rs', 'hs', 'elm'];
+  
+  return (
+    <ul className="flex-center" >
+        {
+          languages.map((l) => (
+            <li key={l}>
+                {/*
+                    we pass an anonymous function here instead of just `this.updateLang(l)
+                    because, otherwise, the expression immediately gets rendered upon eval
+                  */}
+                <button className="btn-clear nav-link"
+                        style={ l === selected ? { color: 'rgb(187, 46, 31)' } : null }
+                        onClick={ () => updateLang(l) }>
+                    { l }
+                </button>
+            </li>
+          ))
+        }
+    </ul>
+  )  
+}
+
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -15,28 +39,15 @@ export default class Popular extends React.Component {
     })
   }
   render() {
-    const languages = ['all', 'js', 'rb', 'java', 'css', 'py', 'rs', 'hs', 'elm'];
-    
+    const { selected } = this.state;
     return (
-      <ul className="flex-center" >
-          {
-          languages.map((l) => (
-          <li key={l}>
-              {/*
-              we pass an anonymous function here instead of just `this.updateLang(l)
-              because, otherwise, the expression immediately gets rendered upon eval
-              */}
-              <button className="btn-clear nav-link"
-                      style={ l === this.state.selected ? { color: 'rgb(187, 46, 31)' } : null }
-                      onClick={ () => this.updateLang(l) }>
-                  { l }
-              </button>
-          </li>
-          ))
-          }
-      </ul>
+      <React.Fragment>
+          <LangNav selected={ selected }
+                   updateLang={ this.updateLang }>
+          </LangNav>
+      </React.Fragment>
+      
     )
   }
 }
-
 
