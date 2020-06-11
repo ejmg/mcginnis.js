b351167ec794cb9d4d48ac633d1870696acd3a1e
elias <elias.jm.garcia@gmail.com>
2020-06-11 15:18:22 -0500

    added navlink state

--- gh-battle/app/components/Popular.js
+++ gh-battle/app/components/Popular.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 'all'
+    };
+    this.updateLang = this.updateLang.bind(this);
+  }
+
+  updateLang(lang) {
+    this.setState({
+      selected: lang
+    })
+  }
   render() {
     const languages = ['all', 'js', 'rb', 'java', 'css', 'py', 'rs', 'hs', 'elm'];
     
     return (
       <ul className="flex-center" >
           {
-            languages.map((l) => (
-              <li key={l}>
-                  <button className="btn-clear nav-link">
-                      { l }
-                  </button>
-              </li>
-            ))
+          languages.map((l) => (
+          <li key={l}>
+              {/*
+              we pass an anonymous function here instead of just `this.updateLang(l)
+              because, otherwise, the expression immediately gets rendered upon eval
+              */}
+              <button className="btn-clear nav-link"
+                      style={ l === this.state.selected ? { color: 'rgb(187, 46, 31)' } : null }
+                      onClick={ () => this.updateLang(l) }>
+                  { l }
+              </button>
+          </li>
+          ))
           }
       </ul>
     )
