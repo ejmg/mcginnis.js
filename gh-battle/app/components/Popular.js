ccc7dcd6425dac47e2f7918aa5df294a16f200bb
elias <elias.jm.garcia@gmail.com>
2020-06-14 15:03:11 -0500

    finished grid layout

--- gh-battle/app/components/Popular.js
+++ gh-battle/app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 
 function LangNav({ selected, updateLang}) {
   const languages = ['all', 'js', 'ruby', 'java', 'css', 'python', 'rust', 'haskell', 'elm'];
@@ -46,22 +47,42 @@ function ReposGrid ({ repos }) {
                    alt={`Avatar for ${login}`}
                    src={ avatar_url }/>
               <h2 className="center-text ">
-                  <a href={ html_url }>
+                  <a className="link" href={ html_url }>
                       { login }
                   </a>
               </h2>
+              <ul className="card-list">
+                  <li>
+                      <FaUser color="rgb(255, 191, 116)" size={ 22 } />
+                      <a href={ `http://github.com${login}` }>
+                          {login}
+                      </a>
+                  </li>
+                  <li>
+                      <FaStar color="rgb(255, 215, 0)" size={ 22 } />
+                      { stargazers_count.toLocaleString() } stars
+                  </li>
+                  <li>
+                      <FaCodeBranch color="rgb(129, 195, 245)" size={ 22 } />
+                      { forks.toLocaleString() } forks
+                  </li>
+                  <li>
+                      <FaExclamationTriangle color="rgb(241, 138, 147)" size={ 22 } />
+                      { open_issues.toLocaleString() } open issues
+                  </li>
+              </ul>
           </li>
-        )
-      })}
+                      )
+    })}
     </ul>
-  )
+    )
 }
 
-ReposGrid.propTypes = {
-  repos: PropTypes.array.isRequired
-}
+    ReposGrid.propTypes = {
+      repos: PropTypes.array.isRequired
+    }
 
-export default class Popular extends React.Component {
+    export default class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
